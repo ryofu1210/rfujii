af3aedccc4ce7bad63c8220d92f8df8280e3dd63
Ryosuke Fujii <ryosuke.fujii@mazrica.com>
2022-10-10 00:46:13 +0900

    add site info

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ryofu1210.github.io',
   integrations: [react(), tailwind({config: {path: './tailwind.config.cjs', applyBaseStyles: false} })]
 });
